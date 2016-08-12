cd4b46209e2f8ac34bb9d378a9e83bf0adec38fa
Alex Hernandez <alexh385@gmail.com>
2016-08-12 00:47:28 -0400

    Add bootstrap html to appropriate render functions in JSX
    Add 'Menu' component that will
    render the sliding menu and set its
    visibility state
    
    Add 'MenuItem' component to handle
    the items in the sliding menu
    
    Add line in 'SearchableInvestmentTable'
    to show sliding menu when inputted
    symbol is submitted
    
    Add condition to make sure the
    current stock is owned and the quantity
    being sold is not greater than the
    quantity purchased before allowing sale
    
    Add alert if user is trying to sell
    more than they own.
    
    	modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var investmentRows;
+var investmentRows, investBtns;
 
 var CurrentPortfolioRow = React.createClass({
 	render: function() {
@@ -19,7 +19,6 @@ var InvestmentRow = React.createClass({
 				<td id="name">{this.props.stockName}</td>
 				<td id="shares">{this.props.shares}</td>
 				<td id="paid">{this.props.askPrice}</td>
-				<input className="currentPortfolioData" type="button" value="View Stock"></input>
 			</tr>
 		);
 	}
@@ -32,19 +31,24 @@ var InvestmentTable = React.createClass({
 			investmentRows.push(<InvestmentRow id={investment.symbol} stockName={investment.company} askPrice={investment.purchasePrice} shares={investment.quantity} />)
 		});
 		return (
-			<table className="portfolio">
-				<thead>
-					<CurrentPortfolioRow cash={this.props.cash} />
-				</thead>
-				<thead>
-					<th className="currentPortfolioHeader">Company</th>
-					<th className="currentPortfolioHeader">Quantity</th>
-					<th className="currentPortfolioHeader">Price Paid</th>
-				</thead>
-				<tbody id="rows">
-					{investmentRows}
-				</tbody>
-			</table>
+			<div className="table-responsive_2">
+				<table className="table table-striped">
+					<thead>
+						<CurrentPortfolioRow cash={this.props.cash} />
+					</thead>
+					<thead>
+						<tr>
+							<th>Company</th>
+							<th>Quantity</th>
+							<th>Price Paid</th>
+							<th>Profit/Loss</th>					
+						</tr>
+					</thead>
+					<tbody id="rows">
+						{investmentRows}
+					</tbody>
+				</table>
+			</div>
 		);
 	}
 });
@@ -63,17 +67,21 @@ var InvestmentPortfolioTable = React.createClass({
 var CurrentStockTable = React.createClass({
 	render: function() {
 		return (
-			<div>
-				<table className="currentStock">
+			<div className="table-responsive">
+				<table className="table table-striped">
 					<thead>
-						<th>{this.props.stockName}</th>
-						<th> {this.props.symbol}</th>
+						<tr>
+							<th>{this.props.stockName}</th>
+							<th> {this.props.symbol}</th>
+						</tr>
 					</thead>
-					<tbody>
+					<thead>
 						<tr>
 							<th>Bid</th>
 							<th>Ask</th>
 						</tr>
+					</thead>
+					<tbody>
 						<tr>
 							<td>
 								{this.props.bid}
@@ -109,20 +117,37 @@ var SearchBar = React.createClass({
 	},
 	render: function() {
 		return (
-			<form onSubmit={this.handleSubmit}>
-				<input 
-					value="Search" 
-					type="submit" 
-					className="symbolInput"
-				/>
-				<input 
-					type="text" 
-					value={this.state.symbols}
-					onChange={this.handleSymbolChange} 
-					placeholder="Enter Stock Symbol" 
-					className="symbolInput" 
-				/>
-			</form>
+			<nav className="navbar navbar-inverse navbar-fixed-top">
+	        	<div className="container-fluid">
+		          	<div className="navbar-header">
+		            	<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+		              		<span className="sr-only">Toggle navigation</span>
+		              		<span className="icon-bar"></span>
+		              		<span className="icon-bar"></span>
+		              		<span className="icon-bar"></span>
+		            	</button>
+		            	<a className="navbar-brand" href="#">The Simple Investment Simulator</a>
+		          	</div>
+					<div id="navbar" className="navbar-collapse collapse">
+						<form className="navbar-form navbar-right" onSubmit={this.handleSubmit}>
+							<input 
+								type="text" 
+								value={this.state.symbols}
+								onChange={this.handleSymbolChange} 
+								placeholder="Enter Stock Symbol..." 
+								className="form-control"
+								id="invest" 
+							/>
+							<input
+								id="currentStock" 
+								value="Lookup" 
+								type="submit" 
+								className="form-control"
+							/>
+						</form>
+					</div>
+				</div>
+			</nav>
 		);
 	}
 });
@@ -165,10 +190,51 @@ var TransactionBar = React.createClass({
 				/>
 				<input 
 					type="submit" 
-					value="Buy" 
+					value="Buy"
 				/>
-				<input type="button" value="Sell" onClick={this.handleSale} />
+				<input 
+					type="button" 
+					value="Sell"
+					onClick={this.handleSale} />
 			</form>
+		);
+	}
+});
+
+var Menu = React.createClass({
+	getInitialState: function() {
+		return (
+			{
+				visible: false
+			}
+		);
+	},
+	show: function() {
+		investBtns = document.getElementById('invest');
+		this.setState({visible: true});
+		investBtns.addEventListener("click", this.hide.bind(this));
+	},
+	hide: function() {
+		this.setState({visible: false});
+	},
+	render: function() {
+		return (
+			<div className="menu">
+				<div className={(this.state.visible ? "visible" : "") + this.props.alignment}>{this.props.children}</div>
+			</div>
+		);
+	}
+});
+
+var MenuItem = React.createClass({
+	navigate: function(hash) {
+		window.location.hash = hash;
+	},
+	render: function() {
+		return (
+			<div className="menu-item" onClick={this.navigate.bind(this, this.props.hash)}>
+				{this.props.children}
+			</div>
 		);
 	}
 });
@@ -214,6 +280,8 @@ var SearchableInvestmentTable = React.createClass({
 				alert('No stock found!')
 			}.bind(this)
 		});
+		// This will display the sliding menu on submit
+		this.refs.left.show();
 	},
 	handleInvestment: function(quantity) {
 		var quantity = parseInt(quantity.shares);
@@ -248,7 +316,8 @@ var SearchableInvestmentTable = React.createClass({
 		var symbol = this.state.symbol;
 		var investments = this.state.investments;
 		for (var i = 0; i < investments.length; i++) {
-			if (investments[i].company === currentStock) {
+			if (investments[i].company === currentStock &&
+				quantity <= investments[i].quantity) {
 				var investment = document.getElementById(symbol);
 				$(investment).find('#shares').text(investments[i].quantity -= quantity);
 				this.setState({
@@ -256,30 +325,59 @@ var SearchableInvestmentTable = React.createClass({
 				});
 				if (investments[i].quantity <= 0) {
 					investment.remove();
+					investments[i] = {};
 				}
-			}  
+			} else if (investments[i].company === currentStock && investments[i].quantity < quantity) {
+				alert("You cannot sell what you don't have!");  
+		  	}
 		}
 	},
 	render: function() {
 		return (
 			<div>
 				<SearchBar onSymbolSubmit={this.handleSymbolSubmit} />
-				<CurrentStockTable 
-					stockName={this.state.stockName}
-					symbol={this.state.symbol}
-					bid={this.state.bidPrice}
-					ask={this.state.askPrice}  
-				/>
-				<TransactionBar 
-					company={this.state.stockName}
-					onBuySubmit={this.handleInvestment}
-					investments={this.state.investments}
-					onSellSubmit={this.handleInvestmentSale} 
-				/>
-				<InvestmentTable 
-					investments={this.state.investments}
-					cash={this.state.cash}
-				/>
+				<div>
+					<Menu ref="left" alignment="left">
+						<ul className="nav nav-sidebar">
+							<li className="active"><a href="#">Ready to invest?<span className="sr-only">(current)</span></a></li>
+						</ul>
+						<MenuItem hash="first-page">
+							<CurrentStockTable 
+								stockName={this.state.stockName}
+								symbol={this.state.symbol}
+								bid={this.state.bidPrice}
+								ask={this.state.askPrice}  
+							/>
+							<TransactionBar 
+								company={this.state.stockName}
+								onBuySubmit={this.handleInvestment}
+								investments={this.state.investments}
+								onSellSubmit={this.handleInvestmentSale} 
+							/>
+						</MenuItem>
+					</Menu>
+				</div>
+				<div className="container-fluid">
+			        <div className="row">
+											
+				        <div className="col-sm-8 col-sm-offset-3 col-md-8 col-md-offset-2 main">
+				            <h1 className="page-header">Your Portfolio</h1>
+
+				            <div className="row placeholders">
+				              	<div className="col-xs-10 col-sm-12 placeholder">
+				                	<img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" className="img-responsive" alt="Generic placeholder thumbnail" />
+				        		</div>
+			          		</div>
+							<div className="portfolio" >
+						        <h2 className="sub-header">Investments</h2>
+								<InvestmentTable 
+									investments={this.state.investments}
+									cash={this.state.cash}
+								/>
+							</div>
+						</div>
+					</div>
+				</div>		
 			</div>
 		);
 	}
@@ -297,6 +395,16 @@ ReactDOM.render(
 
 // Alert if cash will go negative if purchase is made
 
-// Alert if quantity being sold is not available.
+
+// Bugs
+
+// If multiple investments are made in the same company the app does not recognize that they should be grouped together
+// Write a condition that checks whether the company already exists in the investment table.
+// If so then it should NOT add a new row but instead add the quantity to the existing stock Object
+// It should also add the price paid to the purchase price of the existing object and divide it by two
+
+
+
+
 
 
